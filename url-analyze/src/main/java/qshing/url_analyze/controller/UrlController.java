f3d7d3822d1813ae1179ffe3527f8872eaeccb94
seunghye <129871487+bsh8798@users.noreply.github.com>
2025-02-19 22:19:33 +0900

    Add files via upload

---
+++ url-analyze/src/main/java/qshing/url_analyze/controller/UrlController.java
@@ -0,0 +1,32 @@
+package qshing.url_analyze.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import qshing.url_analyze.dto.RequestDTO;
+import qshing.url_analyze.service.UrlAnalysisService;
+
+@RestController
+@RequiredArgsConstructor
+public class UrlController {
+    private final UrlAnalysisService urlAnalysisService;
+
+    @PostMapping("/analyze")
+    public String analyzeURL(@RequestBody RequestDTO request) {
+        //요청에서 url 추출
+        String url = request.getText();
+        //System.out.println(url);
+
+        //url 객체 생성
+        urlAnalysisService.setUrlDTO(url);
+
+        //url이 안전하다고 판단된 경우 result = false
+        boolean result = urlAnalysisService.checkDangerous();
+        //System.out.println(result);
+        if(result)
+            return "true";
+        else
+            return "false";
+    }
+}
---
+++ url-analyze/src/main/java/qshing/url_analyze/dto/UrlDTO.java
@@ -0,0 +1,13 @@
+package qshing.url_analyze.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UrlDTO {
+    private String url;
+    private boolean isPossibleDNS;
+}
