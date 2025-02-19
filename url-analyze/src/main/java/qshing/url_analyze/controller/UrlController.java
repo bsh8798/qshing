1479fc9bf1fb0b57f5a582bd353055810076d68d
seunghye <129871487+bsh8798@users.noreply.github.com>
2025-02-19 20:34:21 +0900

    Add files via upload

---
+++ url-analyze/src/main/java/qshing/url_analyze/controller/UrlController.java
@@ -0,0 +1,28 @@
+package qshing.url_analyze.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import qshing.url_analyze.dto.RequestDTO;
+import qshing.url_analyze.service.UrlAnalysisService;
+
+@RestController
+@RequiredArgsConstructor
+public class UrlController {
+    private final UrlAnalysisService urlAnalysisService;
+
+    @PostMapping("/analyze")
+    public void analyzeURL(@RequestBody RequestDTO request) {
+        //요청에서 url 추출
+        String url = request.getText();
+
+        //url 객체 생성
+        urlAnalysisService.setUrlDTO(url);
+
+        //url이 안전하다고 판단된 경우 result = false
+        boolean result = urlAnalysisService.checkDangerous();
+    }
+}
